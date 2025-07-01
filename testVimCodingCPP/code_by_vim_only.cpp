80f097fbaa907338ba1e91e39c5a3bde28c34482
sugar <2247945677@cug.edu.cn>
2025-07-01 13:15:13 +0800

    测试vim写c++

---
+++ testVimCodingCPP/code_by_vim_only.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "不装插件不舒服" << endl;
+	return 0;
+}
---
+++ testVimCodingCPP/plugin_code_cpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "好用点了" << std::endl;
+    return 0;
+}
+
+
